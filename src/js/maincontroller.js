8cf45c20cd57cc66164460a6db409cbe2461e285
Caleb Floyd <ctfloyd@wisc.edu>
2018-10-15 22:00:18 -0500

    worked on some frontend, rudimentary todolist implemented

--- src/js/maincontroller.js
+++ src/js/maincontroller.js
@@ -1,3 +1,17 @@
-app.controller("MainController", function($scope){
+app.controller("MainController", function ($scope) {
     $scope.inputValue = "";
- });
+    $scope.toDo = [];
+
+    $scope.updateList = function () {
+        $scope.toDo.push($scope.inputValue);
+        $scope.inputValue = "";
+    };
+
+    $scope.hover = function (hoverObject) {
+        angular.element(hoverObject.target).addClass('bg-secondary');
+    };
+
+    $scope.noHover = function (hoverObject) {
+        angular.element(hoverObject.target).removeClass('bg-secondary');
+    };
+});
